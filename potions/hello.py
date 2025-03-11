e59e93f5f406dfb5060215a568dfef5edb1e8959
Tytus Woodburn <tytus.woodburn@student.cune.edu>
2025-03-11 09:45:52 -0500

    Added virtual environment, dependencies and init app.

---
+++ potions/hello.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from potions!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ potions/potion/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PotionConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'potion'
